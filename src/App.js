98461a9a286efff34fbafe8cf3af03fe74e09b0c
seth-bradshaw <seth.bradshaw27@gmail.com>
2020-11-16 17:58:40 -0700

    Got some styling done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import PrivateRoute from './components/PrivateRoute'
 import Organizer_Home from './components/Organizer_Home'
 import Login from './components/Login'
@@ -10,11 +10,12 @@ function App() {
     <Router>
     <div className="App">
       <header className="App-header">
-        <h1>Potluck Planner</h1> 
-        <Route exact path='/' component={Login} />
-        <Route path='/register' component={Register} />
-        <PrivateRoute exact path='/home_page' component={Organizer_Home} />
+        <h1>Potluck Planner</h1>
+        <Link to='/register'/>
       </header>
+      <Route exact path='/' component={Login} />
+      <Route path='/register' component={Register} />
+      <PrivateRoute exact path='/home_page' component={Organizer_Home} />
     </div>
     </Router>
   );
