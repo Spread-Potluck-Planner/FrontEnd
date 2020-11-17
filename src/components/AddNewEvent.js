703f066859dfb5a4768b36fe99eab6055baf215d
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-11-17 11:45:44 -0800

    Form is loggin data to console.

--- src/components/AddNewEvent.js
+++ src/components/AddNewEvent.js
@@ -1,31 +1,63 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import {Button, FormGroup,Label, Input, Textarea} from 'reactstrap'
+import {Button, FormGroup, Input, Form} from 'reactstrap'
 
 import Navigation from './Navigation'
 
 const AddNewEvent = () => {
-    const { register, handleSubmit, errors } = useForm();
+    const { register, handleSubmit, errors, reset } = useForm();
     const onSubmit = data => console.log(data);
-    console.log(errors);
+    
   
     return (
         <div className='dashboard'>
             <Navigation />
             <div className='dashboard-cards'>
-            <form onSubmit={handleSubmit(onSubmit)} className='add-event-container'>
+            <Form onSubmit={handleSubmit(onSubmit)} className='add-event-container'>
+                
+                <input type="text" 
+                placeholder="Name" 
+                name="Name" 
+                ref={register({required: true, maxLength: 80})    } 
+                />
+                
+                <Input type="text" 
+                placeholder="Date" 
+                name="Date" 
+                innerRef={register({required: true, maxLength: 100})   } 
+                />
+                <Input type="text" 
+                placeholder="Time" 
+                name="Time" 
+                innerRef={register({required: true, maxLength: 10})    } 
+                />
+                <Input type='textarea' 
+                placeholder="Description goes here" 
+                name="Description" 
+                innerRef={register({required: true})} 
+                />
+                <Input type="text" 
+                placeholder="Address" 
+                name="Address" 
+                innerRef={register({required: true})} 
+                />
+                <Input type="text" 
+                placeholder="City" 
+                name="City" 
+                innerRef={register({required: true})} 
+                />
+                <Input type="text" 
+                placeholder="State" 
+                name="State" 
+                innerRef={register({required: true})} 
+                />
                 <FormGroup>
-                <Input type="text" placeholder="Name" name="Name" ref={register({required: true, maxLength: 80})    } />
+                <Button color ='primary' type="submit" style={{width:'30%', margin: '20px 10px 20px 10px', }}>Submit</Button>
+                <Button onClick={() => reset()} color='danger' style={{width:'30%', margin: '20px 10px 20px 10px', }}>Clear</Button>
+                
+                
                 </FormGroup>
-                <Input type="text" placeholder="Date" name="Date" ref={register({required: true, maxLength: 100})   } />
-                <Input type="text" placeholder="Time" name="Time" ref={register({required: true, maxLength: 10})    } />
-                <Input type='textarea' placeholder="Description goes here" name="Description" ref={register({required: true})} />
-                <Input type="text" placeholder="Address" name="Address" ref={register({required: true})} />
-                <Input type="text" placeholder="City" name="City" ref={register({required: true})} />
-                <Input type="text" placeholder="State" name="State" ref={register({required: true})} />
-    
-                <Button color ='primary' type="submit" style={{width:'50%', margin: '20px auto 20px auto', }}>Submit</Button>
-            </form>
+            </Form>
           
           
           
