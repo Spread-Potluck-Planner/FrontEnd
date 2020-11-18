f5b223bc931c23ba6e153d91c123222702988c3c
seth-bradshaw <seth.bradshaw27@gmail.com>
2020-11-18 15:17:22 -0700

    finished single event styling and added delete event functionality.

--- src/components/SinglePotluck.js
+++ src/components/SinglePotluck.js
@@ -4,12 +4,15 @@ import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, Ca
 import {useParams, useHistory} from 'react-router-dom'
 import Navigation from '../components/Navigation'
 import {fetchEvent} from '../actions/userActions'
+import food_logo from '../assets/food_img.jpg'
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 import classnames from 'classnames'
 // import {fetchGuests} from '../actions/userActions'
 import '../styling/SinglePotluck.css'
 
 export default function SinglePotluck() {
     const event = useSelector(state => state.eventState);
+    console.log('here is the event data', event)
     const dispatch = useDispatch()
     // const guests = useSelector(state => state.guestsState)
     const [activeTab, setActiveTab] = useState('1');
@@ -25,36 +28,55 @@ export default function SinglePotluck() {
     const toggle = tab => {
         if(activeTab !== tab) setActiveTab(tab)
     }
+
+    const deleteEvent = () => {
+        axiosWithAuth()
+            .delete(`https://potluck-planner-bw.herokuapp.com/events/${id}`)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        push(`/user/${event.organizer_id}`)
+    }
     
     return (
         <div className='dashboard'>
             <Navigation />
             <div className='single-page-potluck'>
             <Card>
-                <CardBody>
-                    <CardTitle tag="h5">{event.event_name.charAt(0).toUpperCase() + event.event_name.slice(1)}</CardTitle>
-                </CardBody>
-                <img width="100%" src="../assets/logo.png" alt="Card image cap" />
-                <CardBody>
-                    <CardText>
-                        Location: {
-                        event.address 
-                        + ', ' + 
-                        event.city.charAt(0).toUpperCase() + event.city.slice(1)
-                        + ', ' + 
-                        event.state.charAt(0).toUpperCase() + event.state.slice(1)
-                        }   
-                    </CardText>
-                    <CardText>
-                        Date: {
-                        event.time 
-                        + ' ' +
-                        eventDate
-                        }
-                    </CardText>
-                </CardBody>
-                <Button type='button' onClick={ () => push(`/events/edit/${id}`) } color='secondary'>Edit</Button>
-                <Button type='button' onClick={() => push(`/user/${id}`)} color='danger' >Cancel</Button>
+                <div className='targeting-background-div'>
+                    <CardBody className='header-card-body'>
+                        <CardTitle tag="h4">{event.event_name.charAt(0).toUpperCase() + event.event_name.slice(1)}</CardTitle>
+                    </CardBody>
+                    <img src={food_logo} alt="Card image cap" />
+                    {/* Key: pk.eyJ1IjoiZXRoYnJhZHNoYXciLCJhIjoiY2tobnNhcXNhMGExczJ5bWlmMnllNjNyYSJ9.9yVsCdIiqTfD8B5xo_WEmQ */}
+                    <CardBody>
+                        <CardText id='single-event-p'>
+                            <span className='event-info-labels'>Location:</span> {
+                            event.address 
+                            + ', ' + 
+                            event.city.charAt(0).toUpperCase() + event.city.slice(1)
+                            + ', ' + 
+                            event.state.charAt(0).toUpperCase() + event.state.slice(1)
+                            }   
+                        </CardText>
+                        <p className='event-info-border'></p>
+                        <CardText id='single-event-p'>
+                        <span className='event-info-labels'>Date:</span> {
+                            event.time 
+                            + ' ' +
+                            eventDate
+                            }
+                        </CardText>
+                    </CardBody>
+                    <div className='single-page-button'>
+                        <Button type='button' id='edit-btn' onClick={ () => push(`/events/edit/${id}`) } color='secondary'>Edit</Button>
+                        <Button type='button' id='delete-btn' onClick={ () => deleteEvent() } color='danger' >Delete</Button>
+                        <Button type='button' id='back-btn' onClick={ () => push(`/user/${event.organizer_id}`) } color='danger' >Back</Button>
+                    </div>
+                </div>
                 <Nav tabs>
                     <NavItem>
                         <NavLink
@@ -77,13 +99,13 @@ export default function SinglePotluck() {
                     <TabPane tabId='1' >
                         <Row>
                             <Col sm='12'>
-                            <h4>Guests:</h4>
+                            <h6>Guests</h6>
                             {event.guests.map(guest => {
-                                return <p key={guest.user_id}>{guest.full_name.charAt(0).toUpperCase() + guest.full_name.slice(1)} 
+                                return <p key={guest.user_id} id='styled-p-nav'>{guest.full_name.charAt(0).toUpperCase() + guest.full_name.slice(1)} 
                                 {/* dont know if this will work but can't test it */}
                                 {typeof event.recipes !== 'undefined' && event.recipes.filter(recipe => {
                                     if(guest.user_id === recipe.user_id){
-                                        return <p>{':' + recipe}</p>
+                                        return <p id='styled-p-nav'>{':' + recipe}</p>
                                     }
                                 })}</p>
                             })}
@@ -93,30 +115,13 @@ export default function SinglePotluck() {
                     <TabPane tabId='2'>
                         <Row>
                             <Col sm='12'>
-                                <h4>Description:</h4>
+                                <h6>Description</h6>
                                 <p>{event.description}</p>
                             </Col>
                         </Row>
                     </TabPane>
                 </TabContent>
             </Card>
-                {/* <h2>{event.event_name.charAt(0).toUpperCase() + event.event_name.slice(1)}</h2>
-                <h4>
-                    Location: {
-                    event.address 
-                    + ', ' + 
-                    event.city.charAt(0).toUpperCase() + event.city.slice(1)
-                    + ', ' + 
-                    event.state.charAt(0).toUpperCase() + event.state.slice(1)
-                    }
-                </h4>
-                <h4>
-                    Date: {
-                    event.time 
-                    + ' ' +
-                    eventDate
-                    }
-                </h4> */}
             </div>
         </div>
     )
