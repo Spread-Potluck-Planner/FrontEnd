c0680ba7fd1c665609ceb05ec4020745caf33706
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-11-19 22:19:58 -0800

    Can edit user

---
+++ src/components/EditUserForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import {useHistory, useParams} from 'react-router-dom';
+import { ErrorMessage } from '@hookform/error-message';
+import {Form, Button, Label, Input, FormGroup} from 'reactstrap';
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+
+import Navigation from './Navigation'
+
+const EditUserForm = () => {
+      const {push} = useHistory();
+      const {id} = useParams()
+      const { register, handleSubmit, errors, reset } = useForm({ 
+        mode: "onBlur",
+  
+      });
+
+      const onSubmit = (data) => {
+        updateUser(data)
+      }
+  
+      const updateUser= (user) => {
+          axiosWithAuth()
+              .put(`https://potluck-planner-bw.herokuapp.com/users/${id}`, user)
+              .then(res => {
+                  alert("Your name has been updated.")
+                  push('/')
+              })
+              .catch(err => {
+                  console.log(err)
+              })
+      }
+  
+      return (
+
+        <div className='dashboard'>
+            <Navigation />
+            <div className='dashboard-cards'>
+                <h2>Update Account Details</h2>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                <FormGroup id='edit-form-group'>
+                    <Label for='full_name'>Name</Label>
+                    <Input type="text" 
+                    placeholder="Full Name" 
+                    name="full_name" 
+                    invalid={errors.full_name ? true : false}
+                    innerRef={register({required: "You must make a change to update your account.", maxLength: 80})    } 
+                />
+                  </FormGroup>
+                  <ErrorMessage errors={errors} name='full_name' />
+                  <div id='btn-group'>
+                    <Button type='submit' color='warning'>Submit</Button>
+                    <Button type='button' color='danger' onClick={() => push('/')}>Cancel</Button>
+                  </div>
+              </Form>
+            </div>
+        </div>    
+      )
+}
+ 
+export default EditUserForm;
